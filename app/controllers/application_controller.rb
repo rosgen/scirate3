77a4e39f90b4761716435296652d7706486cf738
Bill Rosgen <rosgen@gmail.com>
2013-07-03 23:32:19 +0800

    Remove notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,10 @@ class ApplicationController < ActionController::Base
 
   before_filter :redirect_https
 
-  before_filter :add_notice
-
   def redirect_https
     if (!request.ssl? && Rails.env.production?)
       redirect_to protocol: "https://", host: ENV['HOST']
     end
     return true
-  end
-
-  def add_notice
-    flash[:alert] = 'Notice: arxiv.org is experiencing technical difficulties regarding automated access.  Scirate will be updated once these issues are resolved.'
   end
 end
